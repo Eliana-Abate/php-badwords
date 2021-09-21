0ca655e0009ca99f01b71d0bd8ccaf79c051a681
Eliana-Abate <eliana.abate@outlook.it>
2021-09-21 16:23:04 +0200

    replaced badword

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
     $paragraph = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis quos quae et ea delectus officia ipsa molestias laborum repellat ipsum voluptas, hic a eius impedit numquam minus. Doloribus, rem tempore.';
+    $bad_word = $_GET['bad_word'];
+     
+    $new_par = str_replace('adipisicing', '(***)', $paragraph);
+  
+    
 ?>
 
 
@@ -12,11 +17,18 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-    <?php echo $paragraph ; 
+    <?php echo $paragraph; 
         echo '<br/>';
         echo 'Lunghezza stringa: ' . strlen($paragraph);
-        
+        echo '<br/>';
         var_dump($paragraph);
+        
+        echo '<hr/>';
+
+        echo $new_par;
+        echo '<br/>';
+        echo 'Lunghezza nuova stringa: ' . strlen($new_par);
+        var_dump($new_par);
         
     ?>
     
